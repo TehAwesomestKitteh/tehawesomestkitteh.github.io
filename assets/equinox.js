94cbd125e0c069881e049f37bf76cc8767c70eaf
TehAwesomestKitteh <jorem9@gmail.com>
2016-12-25 11:43:46 +0800

    Update equinox.js

--- assets/equinox.js
+++ assets/equinox.js
@@ -31,12 +31,25 @@ function update() {
     document.getElementById("days_hundreds").src = "assets/" + String(days_hundreds) + ".gif";
     document.getElementById("days_tens").src = "assets/" + String(days_tens) + ".gif";
     document.getElementById("days").src = "assets/" + String(days) + ".gif";
+    document.getElementById("days_line").src = "assets/active.gif";
+  };
+  
+  if (getTimeRemaining(deadline).hours > 0) {
     document.getElementById("hrs_tens").src = "assets/" + String(hrs_tens) + ".gif";
     document.getElementById("hrs").src = "assets/" + String(hrs) + ".gif";
+    document.getElementById("hrs_line").src = "assets/active.gif";
+  };
+  
+  if (getTimeRemaining(deadline).minutes > 0) {
     document.getElementById("mins_tens").src = "assets/" + String(mins_tens) + ".gif";
     document.getElementById("mins").src = "assets/" + String(mins) + ".gif";
+    document.getElementById("mins_line").src = "assets/active.gif";
+  };
+  
+  if (getTimeRemaining(deadline).seconds > 0) {
     document.getElementById("secs_tens").src = "assets/" + String(secs_tens) + ".gif";
     document.getElementById("secs").src = "assets/" + String(secs) + ".gif";
+    document.getElementById("secs_line").src = "assets/active.gif";
   };
   
   // Handles tick-tock
