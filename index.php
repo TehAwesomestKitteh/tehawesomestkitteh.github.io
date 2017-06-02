d6854e8eba19e72e0c141e6511522e9e9a51891a
TehAwesomestKitteh <jorem9@gmail.com>
2017-06-03 00:33:57 +0800

    Revert "Initial testing overhaul of HTML to PHP"
    This reverts commit 86a77ac3d804700234b8c715ba70f2522f651924.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include("modules/header/php"); ?>
-
-			<img src="Order/1a.gif">
-			<p class="storyp">...</p>
-			<br>
-			<div id="nextpage"><a href="Order/2">>></a></div>
-		</div>
-	</body>
-</html>
